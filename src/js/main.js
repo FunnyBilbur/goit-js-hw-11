c8acebc1aef7e798b10adac32867447961a72ee0
FunnyBilbur <tonistark1997@gmail.com>
2024-01-03 08:41:10 +0300

    Done

--- src/js/main.js
+++ src/js/main.js
@@ -4,103 +4,88 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 const form = document.querySelector('.form');
 const input_search = document.querySelector('.input__search');
 const gallery = document.querySelector(".gallery");
-
+const load = document.querySelector(".load");
 
 form.addEventListener('submit', (arg) => {
     arg.preventDefault();
+    gallery.innerHTML = "";
+    load.classList.add('loader');
+    getRandomImages();
+});
+
+function getRandomImages() {
+    let randomPage = Math.floor(Math.random() * 10) + 1;
     let searchParams = new URLSearchParams({
         key: "41527522-465889db431a6a06c19f4d10b",
         q: input_search.value,
         image_type: "photo",
         orientation: "horizontal",
         safesearch: true,
+        page: randomPage,
     });
     fetch(`https://pixabay.com/api/?${searchParams}`)
         .then((response) => {
-            gallery.innerHTML = "";
             if (!response.ok) {
                 throw new Error(response.status);
             }
             return response.json();
         })
         .then((users) => {
-            const markup = users.hits.map(({ largeImageURL, webformatURL, tags, likes, views, comments, downloads }) => {
+            if (users.total + 19 < randomPage * 20) {
+                console.log(`User Total ${users.total} less than RandomPage ${randomPage} and needed count ${randomPage * 20}`);
+                handleEmptyPageResult();
+            }
+            load.classList.remove('loader');
+            if (users.total === 0) {
+                console.log(users);
+                iziToast.show({
+                    titleColor: 'white',
+                    position: 'topRight',
+                    message: 'Sorry, there are no images matching your search query. Please try again!',
+                    messageColor: 'white',
+                    backgroundColor: '#EF4040',
+                    progressBarColor: '#B51B1B'
+                });
+                return;
+            }
+            const markup = users.hits.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
                 return `<li class="gallery__item">
                     <a class="gallery__link" href="${largeImageURL}">
                         <img
                             class="gallery__image"
                             src="${webformatURL}"
                             alt="${tags}"/>
+                        <ul class="info__list">
+                        <li class="info__item">
+                        <span>Likes</span>
+                        <span>${likes}</span>
+                        </li>
+                        <li class="info__item">
+                        <span>Views</span>
+                        <span>${views}</span>
+                        </li>
+                        <li class="info__item">
+                        <span>Comments</span>
+                        <span>${comments}</span>
+                        </li>
+                        <li class="info__item">
+                        <span>Downloads</span>
+                        <span>${downloads}</span>
+                        </li>
+                        </ul>
+            
                     </a>
             </li>`;
             })
                 .join("");
-
             gallery.insertAdjacentHTML("beforeend", markup);
             var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: '250' /* options */ });
+            lightbox.refresh();
             console.log(users);
-
         })
         .catch((error) => console.log(error));
-});
+}
 
-// const fetchUsersBtn = document.querySelector(".btn");
-
-
-// fetchUsersBtn.addEventListener("click", () => {
-//     fetch("https://jsonplaceholder.typicode.com/users?_limit=4&_sort=name")
-//         .then((response) => {
-//             if (!response.ok) {
-//                 throw new Error(response.status);
-//             }
-//             return response.json();
-//         })
-//         .then((users) => {
-//             const markup = users.map((user) => {
-//                 return `<li>
-// 	          <p><b>Name</b>: ${user.name}</p>
-// 	          <p><b>Email</b>: ${user.email}</p>
-// 	          <p><b>Company</b>: ${user.company.name}</p>
-// 	        </li>`;
-//             })
-//                 .join("");
-
-//             userList.insertAdjacentHTML("beforeend", markup);
-//         })
-//         .catch((error) => console.log(error));
-// });
-
-
-
-
-
-
-// fetchUsersBtn.addEventListener("click", () => {
-//     fetchUsers()
-//         .then((users) => console.log(users))
-//         .catch((error) => console.log(error));
-// });
-
-// function fetchUsers() {
-//     return fetch("https://jsonplaceholder.typicode.com/users").then(
-//         (response) => {
-//             if (!response.ok) {
-//                 throw new Error(response.status);
-//             }
-//             return response.json();
-//         }
-//     );
-// }
-
-// function renderUsers(users) {
-//     const markup = users
-//         .map((user) => {
-//             return `<li>
-//           <p><b>Name</b>: ${user.name}</p>
-//           <p><b>Email</b>: ${user.email}</p>
-//           <p><b>Company</b>: ${user.company.name}</p>
-//         </li>`;
-//         })
-//         .join("");
-//     userList.insertAdjacentHTML("beforeend", markup);
-// }
+function handleEmptyPageResult() {
+    getRandomImages();
+}
